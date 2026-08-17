ffed6157111bb5ca584cb2048476745cfde60c66
rosalexxi <rosalexxi@gmail.com>
2026-08-17 12:45:18 -0300

    Add files via upload

--- bot8.py
+++ bot8.py
@@ -51,7 +51,7 @@ FRANJAS_COMIDAS = {
 
 load_dotenv()
 
-GROQ_TEXTO = "llama-3.1-8b-instruct"
+GROQ_TEXTO = "llama-3.30-70b-versatile"
 GROQ_FOTO = "qwen/qwen3.6-27b"
 
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
