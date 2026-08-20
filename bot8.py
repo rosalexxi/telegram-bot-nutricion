763469f31bad7333deacd07a6f7c076d0ec3ec09
rosalexxi <rosalexxi@gmail.com>
2026-08-20 19:33:32 -0300

    Add files via upload

--- bot8.py
+++ bot8.py
@@ -3553,7 +3553,7 @@ def main():
     if job_queue is not None:
         job_queue.run_daily(
             job_recordatorio_manana, 
-            time=time(hour=19, minute=20, second=0, tzinfo=tz),
+            time=time(hour=19, minute=40, second=0, tzinfo=tz),
             name="recordatorio_comidas_manana"
         )
 
